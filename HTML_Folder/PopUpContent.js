1b7e72e4536e2e15633f6fe7494aab1e2297e4cc
Tahir <tahirafflick@gmail.com>
2023-07-02 12:07:55 +0100

    changes

--- HTML_Folder/PopUpContent.js
+++ HTML_Folder/PopUpContent.js
@@ -195,6 +195,27 @@ if (number)
 // arrays and loops dive into Palindrome.
 
 
+function myFunction(x) {
+
+  x.classList.toggle("change");
+}
 
 
+/*
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}*/
